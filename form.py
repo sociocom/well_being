0fef7217f95e665fddd13f0f0a798654e828ada9
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2022-07-19 23:36:38 +0900

    Update form.py

--- form.py
+++ form.py
@@ -74,10 +74,10 @@ def main():
         df_fb_self=df_fb[df_fb['user']==name]
 
         st.subheader('週間Well-beingスコア')
-        st.caption(
-            '''濃い青線：チームの平均スコア
-            濃い青丸：あなたのスコア
-            うすい青丸：チームの個別スコア'''
+        st.write(
+            '濃い青線：チームの平均スコア\n
+            濃い青丸：あなたのスコア\n
+            うすい青丸：チームの個別スコア'
         )
         line = alt.Chart(df_fb).mark_line(
             color='blue'

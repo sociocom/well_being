9c1f5446da4655e93b24b59b3fabed4a1eff65c6
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2022-07-27 20:20:26 +0900

    Update form.py

--- form.py
+++ form.py
@@ -114,7 +114,7 @@ def main():
         ).encode(
             x=alt.X('date:T'),
             y=alt.Y('my_happy:Q'),
-            size = 'count:Q'
+            size = 'count'
         ).properties(
             width=650,
             height=400
@@ -125,7 +125,7 @@ def main():
         ).encode(
             x=alt.X('date:T'),
             y=alt.Y('my_happy:Q'),
-            size = 'count:Q'
+            size = 'count'
         ).properties(
             width=650,
             height=400

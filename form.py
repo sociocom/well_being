cdde9173ae8bf5fc5992e6037559c4202731bfba
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2022-08-03 00:05:32 +0900

    Update form.py

--- form.py
+++ form.py
@@ -77,8 +77,8 @@ def main():
                             'group_happy':group_happy,
                             'location':location,
                             'location_other':location_other,
-                            'timestamp':str(datetime.datetime.now())},
-                            'team_url':team_url
+                            'timestamp':str(datetime.datetime.now()),
+                            'team_url':team_url}
                             }
 
                 requests.post(url + '/post',json=data_post)

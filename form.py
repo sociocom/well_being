b9204b38d69d504add3b7a541fd6b59d65442afd
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2022-08-03 01:42:26 +0900

    Update form.py

--- form.py
+++ form.py
@@ -23,7 +23,7 @@ diary_list=[]
 url = st.secrets['URL']
 team_url='A'
 
-query_params = st.experimental_get_query_params(selected=["a", "b",'c'])
+query_params = st.experimental_get_query_params()
 
 #st.experimental_set_query_params(selected=["a", "b",'c'])
 

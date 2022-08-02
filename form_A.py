2fe070e41a1ac089f49303d25028ee7d58a697b3
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2022-08-03 00:57:21 +0900

    Update form_A.py

--- form_A.py
+++ form_A.py
@@ -22,7 +22,19 @@ day_list=[]
 diary_list=[]
 url = st.secrets['URL']
 team_url='A'
-st.experimental_get_query_params(){"team": ['A','B','C']}
+
+
+radio_list = ['Eat', 'Sleep', 'Both']
+query_params = st.experimental_get_query_params()
+
+default = int(query_params["activity"][0]) if "activity" in query_params else 0
+activity = st.radio(
+    "What are you doing at home during quarantine?",
+    radio_list,
+    index = default
+)
+
+st.experimental_set_query_params(activity=radio_list.index(activity))
 
 def main():
     with st.expander('パスワード変更はこちら'):

54b62671638bb3df21ca1efb2a6499e6e4d652ad
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2022-08-03 16:00:17 +0900

    Update form.py

--- form.py
+++ form.py
@@ -170,7 +170,7 @@ def main():
 
             st.write(layer)
             
-            st.write('アプリ改善のため、下記のリンクよりご意見・ご感想をお聞かせ下さい \n https://bit.ly/3zwaT3k')
+            st.write('アプリ改善のため、下記のリンクよりご意見・ご感想をお聞かせ下さい /\n https://bit.ly/3zwaT3k')
             st.write()
               
 

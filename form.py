f20087dd378128601612d04150a9a2e7131124a4
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2022-09-05 03:24:02 +0900

    Update form.py

--- form.py
+++ form.py
@@ -344,6 +344,8 @@ elif authentication_status == False:
     st.error('UsernameまたはPasswordが間違っています（英数字・記号は半角にして下さい）')
     st.error('※初期パスワードはUsername（ID）と同様です')
     st.info(message)
+    st.info('（2022/9/5）パスワード変更が反映されない不具合が確認されましたので、修正を行いました。')
 elif authentication_status == None:
     st.warning('UsernameとPasswordをご入力下さい')
     st.info(message)
+    st.info('（2022/9/5）パスワード変更が反映されない不具合が確認されましたので、修正を行いました。')

847d3e49fd5cc8e5284e6513980cfebf38d4b680
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2023-05-13 17:22:06 +0900

    Update fb_demo.py

--- fb_demo.py
+++ fb_demo.py
@@ -108,7 +108,7 @@ def main():
     points = alt.Chart(df_fb).mark_circle(
         color='lightskyblue'
     ).encode(
-        x=alt.X('date:T',axis=None),
+        x=alt.X('date:T',labels=[]),
         y=alt.Y('my_happy:Q'),
         size = 'count()'
     ).properties(

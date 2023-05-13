509654c762d356ea708058ee74228f49afd658b7
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2023-05-13 19:56:28 +0900

    Update fb_demo.py

--- fb_demo.py
+++ fb_demo.py
@@ -118,7 +118,7 @@ def main():
         color='lightskyblue'
     ).encode(
         x=alt.X('date:T',
-                axis=alt.Axis(format="%m/%d",labelFontSize=14, titleFontSize=18,title='日付', scale=alt.Scale(domain=['9/5','9/6','9/7'])),
+                axis=alt.Axis(format="%m/%d",labelFontSize=14, titleFontSize=18,title='日付', labelExpr="datum.value", values=['9/5','9/6','9/7']),
 #                scale=alt.Scale(domainMax={"year": to_day.year, "month": to_day.month, "date": to_day.day},
 #                                domainMin={"year": from_day.year, "month": from_day.month, "date": from_day.day}),
 #                type='temporal'

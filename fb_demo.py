f279439669e298cfe93b1d5c7ad88add3d580bcd
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2023-05-13 20:03:38 +0900

    Update fb_demo.py

--- fb_demo.py
+++ fb_demo.py
@@ -119,9 +119,9 @@ def main():
     ).encode(
         x=alt.X('date:T',
                 axis=alt.Axis(format="%m/%d",labelFontSize=14, titleFontSize=18,title='日付'),
-                scale=alt.Scale(domainMax={"year": to_day.year, "month": to_day.month, "date": to_day.day},
-                                domainMin={"year": from_day.year, "month": from_day.month, "date": from_day.day}),
-                type='temporal'
+                #scale=alt.Scale(domainMax={"year": to_day.year, "month": to_day.month, "date": to_day.day},
+                #                domainMin={"year": from_day.year, "month": from_day.month, "date": from_day.day}),
+                scale = alt.Scale(type='oridinal')
                 ),
         y=alt.Y('mean(my_happy):Q',
                 axis=alt.Axis(titleFontSize=18, title='「あなたの幸せ」スコア'),

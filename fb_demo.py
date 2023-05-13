c7e0ed8cf7bb82624652a6901aef62fbd381bfce
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2023-05-13 16:55:51 +0900

    Update fb_demo.py

--- fb_demo.py
+++ fb_demo.py
@@ -48,10 +48,10 @@ def main():
         run = st.button('集計実行')
 
     from_native = datetime.combine(from_day, time())
-    from_day = pytz.timezone('Pacific/Auckland').localize(from_native)+ timedelta(days=-1)
+    from_day = pytz.timezone('Asia/Tokyo').localize(from_native)+ timedelta(days=-1)
     from_day = datetime.fromordinal(from_day.toordinal())
     to_native = datetime.combine(to_day, time())
-    to_day = pytz.timezone('Pacific/Auckland').localize(to_native)
+    to_day = pytz.timezone('Asia/Tokyo').localize(to_native)
     to_day = datetime.fromordinal(to_day.toordinal())
     
 

486d32462275c0069bdd5c048f62e9c01de8164b
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2023-05-13 20:17:36 +0900

    Update fb_demo.py

--- fb_demo.py
+++ fb_demo.py
@@ -112,13 +112,20 @@ def main():
     ).properties(
         width=750,
         height=400
+    )
+
+    
+    # x軸のデータ型をintervalに変更し、intervalを1日に設定
+    x_interval = alt.Interval(
+        from_=alt.datum['date'],
+        to=alt.datum['date'] + pd.DateOffset(days=1),
         )
         
     line = alt.Chart(df_fb).mark_line(
         color='lightskyblue'
     ).encode(
         x=alt.X('date:T',
-                axis=alt.Axis(format="%m/%d",labelFontSize=14, titleFontSize=18,title='日付'),
+                axis=alt.Axis(interval = x_interval, format="%m/%d",labelFontSize=14, titleFontSize=18,title='日付'),
                 #scale=alt.Scale(domainMax={"year": to_day.year, "month": to_day.month, "date": to_day.day},
                 #                domainMin={"year": from_day.year, "month": from_day.month, "date": from_day.day}),
                 scale = alt.Scale(type='ordinal',aggregate='distinct')

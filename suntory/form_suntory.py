c21aa3889457f421a2b39fb948aac97ee97e5e3e
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2023-12-01 02:03:18 +0900

    Update form_suntory.py

--- suntory/form_suntory.py
+++ suntory/form_suntory.py
@@ -292,7 +292,7 @@ authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
 # ログインメソッドで入力フォームを配置
 st.title(':hatching_chick: 個と場のWell-being日記')
 st.write('''
-:bow: ID・パスワードの登録がまだ完了しておりませんので少々お待ちください。
+:bow: Username・Passwordの登録がまだ完了しておりませんので少々お待ちください。
 12/1(金)15:00頃までに完了予定となります。ご不便をおかけ致します。
 ''')
 name, authentication_status, username = authenticator.login('Login', 'main')
